de267b97f1e5cb2fbcc1f6a69c21807bbc74f3e7
marefpceo <marefpceo@gmail.com>
2023-03-24 13:40:00 -0700

    Completed function up to find();

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ class Node {
 
 class Tree {
   constructor(arr) {
-    this.root = this.buildTree(arr);
+    this.arr = this.arraySort(arr);
+    this.root = this.buildTree(this.arr);
   }
 
   buildTree(arr) {
@@ -28,6 +29,100 @@ class Tree {
     return node;
   }
 
+  // Sorts array and removes duplicates
+  arraySort(arr) {
+    let sortedArray = arr.sort();
+    let arrayNoDuplicates = sortedArray.filter((a, b) => sortedArray.indexOf(a) === b);
+    return arrayNoDuplicates;
+  }
+
+  // Utility search function
+  find(root, key) {
+    if (root === null || root.data === key) {
+      return root;
+    }
+
+    if (root.data < key) {
+      return this.find(root.right, key);
+    } 
+    return this.find(root.left, key);
+  }
+
+  // Inserts a new value into the BST
+  insert(root, key) {
+    if (root === null) {
+      root = new Node(key);
+      return root;
+    }
+
+    if (key < root.data) {
+      root.left = this.insert(root.left, key);
+    } else if (key > root.data) {
+      root.right = this.insert(root.right, key);
+    }
+    return root;
+  }
+
+  // Deletes a value from the BST
+  delete(root, key) {
+    if (root === null) {
+      return root;
+    }
+
+    if (key < root.data) {
+      root.left = this.delete(root.left, key);
+    }
+
+    if (key > root.data) {
+      root.right = this.delete(root.right, key);
+    } else {
+      if (root.left === null) {
+        return root.right;
+      } else if(root.right === null) {
+        return root.left;
+      }
+      root.data = this.minValue(root.right);
+      root.right = this.delete(root.right, root.data);
+    }
+    return root;
+  }
+
+  // Utility function to find smallest value from inorder successort
+  minValue(root) {
+    let min = root.data;
+      while(root.left !== null){
+        min = root.left.data;
+        root = root.left;
+      }
+      return min;
+  }
+
+  // Finds the height of the tree
+  height(root) {
+    if (root === null) {
+      return 0;
+    } else {
+      let left = height(root.left);
+      let right = heigth(root.right);
+
+      if (left > right) {
+        return (left + 1);
+      } else {
+        return (right + 1);
+      }
+    }
+  }
+
+  levelOrder(root, level) {
+    if (root === null) {
+      return;
+    }
+    if (level === 1) {
+      return
+    }
+  }
+
+  // Prints BST in a readable format to the console
   prettyPrint(node, prefix = '', isLeft = true) {
     if (node === null) {
        return;
@@ -40,8 +135,16 @@ class Tree {
       this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
     }
   }
-  
 }
 
-const bst = new Tree([1,2,3,4,5,6,7]);
-bst.prettyPrint(bst.root);
+// const bst = new Tree([1,2,3,4,5,6,7]);
+// const bstSort = new Tree([7,6,5,4,3,2,1]);
+const bstSortDuplicates = new Tree([2,6,5,1,3,6,7,4,1]);
+// bst.prettyPrint(bst.root);
+// bstSort.prettyPrint(bstSort.root);
+bstSortDuplicates.prettyPrint(bstSortDuplicates.root);
+console.log(bstSortDuplicates.find(bstSortDuplicates.root, 6));
+console.log(bstSortDuplicates.insert(bstSortDuplicates.root, 8));
+bstSortDuplicates.prettyPrint(bstSortDuplicates.root);
+console.log(bstSortDuplicates.delete(bstSortDuplicates.root, 4));
+bstSortDuplicates.prettyPrint(bstSortDuplicates.root);
